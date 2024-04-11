c147edbffbf9b5185ad46f491c91facddb684899
ellabbas <ellrabbas@gmail.com>
2024-04-11 19:44:17 +0400

    add artist

---
+++ server/config/firebaseStorage.js
@@ -0,0 +1,23 @@
+const getStorage = require("firebase-admin/storage");
+const initializeApp = require("firebase-admin/app");
+require("dotenv").config();
+
+
+initializeApp({
+    credential: cert({
+        "type": process.env.FIREBASE_TYPE,
+        "project_id": process.env.PROJECT_ID,
+        "private_key_id": process.env.PRIVATE_KEY_ID,
+        "private_key": process.env.PRIVATE_KEY,
+        "client_email": process.env.CLIENT_EMAIL,
+        "client_id": process.env.CLIENT_ID,
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url": process.env.CLIENT_X509_CERT_URL,
+    }),
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+})
+
+const storage = getStorage().bucket();
+module.exports = storage;
